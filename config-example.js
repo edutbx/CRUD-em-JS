4738fd5d0af2214a8acf7e8ba2b0340d30b150e6
edutbx <141964723+edutbx@users.noreply.github.com>
2025-03-26 21:33:21 -0300

    Delete config-example.js

--- config-example.js
+++
@@ -1,6 +0,0 @@
-export const dbConfig = {
-    host: 'localhost',
-    user: 'root', // Substitua pelo seu usuário MySQL
-    password: 'SenhaAlterar', // Substitua pela sua senha
-    database: 'fatec',
-  };
