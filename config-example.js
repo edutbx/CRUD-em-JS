c45a01297f10f8d20cba164ae935dffea3926613
edutbx <eduardo.abreu@fatec.sp.gov.br>
2025-03-26 21:40:57 -0300

    completo

---
+++ config-example.js
@@ -0,0 +1,6 @@
+export const dbConfig = {
+    host: 'localhost',
+    user: 'root', // Substitua pelo seu usuário MySQL
+    password: 'SenhaAlterar', // Substitua pela sua senha
+    database: 'fatec',
+  };
